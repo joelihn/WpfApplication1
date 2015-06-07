05ab1151a9ff79d1c77c915c6398da2c612f255c
joelihn <joelihn@hotmail.com>
2015-06-07 16:11:49 +0800

    设置页弄个二级目录

--- Config.xaml.cs
+++ Config.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApplication1.CustomUI;
 
 namespace WpfApplication1
 {
@@ -21,11 +22,75 @@ namespace WpfApplication1
     public partial class Config : UserControl
     {
         public MainWindow Basewindow;
+        private CPatientArea cPatientArea;
+        private CPatientRoom cPatientRoom;
+        private CBed cBed;
+        private CInfectType cInfectType;
+        private CMedicalOrderPara cMedicalOrderPara;
+        private CTreatType cTreatType;
+        private CTreatMethod cTreatMethod;
+        private CTreatStatus cTreatStatus;
         public Config(MainWindow window)
         {
             InitializeComponent();
             Basewindow = window;
+            cPatientArea = new CPatientArea();
+            cPatientRoom = new CPatientRoom();
+            cBed = new CBed();
+            cInfectType = new CInfectType();
+            cMedicalOrderPara = new CMedicalOrderPara();
+            cTreatType = new CTreatType();
+            cTreatMethod = new CTreatMethod();
+            cTreatStatus = new CTreatStatus();
+            this.RightContent.Content = cPatientArea;
         }
 
+        private void PatientAreaButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            this.RightContent.Content = cPatientArea;
+        }
+
+        private void PatientRoomButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            this.RightContent.Content = cPatientRoom;
+        }
+
+        private void BedButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.RightContent.Content = cBed;
+
+        }
+
+        private void InfectTypeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            this.RightContent.Content = cInfectType;
+        }
+
+        private void MedicalOrderParaButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.RightContent.Content = cMedicalOrderPara;
+
+        }
+
+        private void TreatTypeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.RightContent.Content = cTreatType;
+
+        }
+
+        private void TreatMethodButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.RightContent.Content = cTreatMethod;
+
+        }
+
+        private void TreatStatusButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.RightContent.Content = cTreatStatus;
+
+        }
     }
 }
