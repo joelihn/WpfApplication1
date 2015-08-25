92b8666993c94cbe0d51c78d125f23131430ca42
joelihn <joelihn@hotmail.com>
2015-08-25 21:47:04 +0800

    treat status pages...

--- CustomUI/CTreatStatus.xaml.cs
+++ CustomUI/CTreatStatus.xaml.cs
@@ -25,10 +25,11 @@ namespace WpfApplication1.CustomUI
     {
         public ObservableCollection<TreatStatusData> Datalist = new ObservableCollection<TreatStatusData>();
 
+        private int currnetIndex = -1;
         public CTreatStatus()
         {
             InitializeComponent();
-            this.ListView1.ItemsSource = Datalist;
+            this.ListViewTreatType.ItemsSource = Datalist;
         }
 
         private void ListViewCTreatStatus_OnLoaded(object sender, RoutedEventArgs e)
@@ -46,6 +47,7 @@ namespace WpfApplication1.CustomUI
                         var treatStatusData = new TreatStatusData
                         {
                             Id = type.Id,
+                            Activated = type.Activated,
                             Name = type.Name,
                             Description = type.Description
                         };
@@ -62,10 +64,14 @@ namespace WpfApplication1.CustomUI
         private void ListViewCTreatStatus_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //throw new NotImplementedException();
-            if (ListView1.SelectedIndex >= 0)
+            if (ListViewTreatType.SelectedIndex >= 0)
             {
-                NameTextBox.Text = Datalist[ListView1.SelectedIndex].Name;
-                DescriptionTextBox.Text = Datalist[ListView1.SelectedIndex].Description;
+                currnetIndex = ListViewTreatType.SelectedIndex;
+                this.ButtonApply.IsEnabled = false;
+                this.ButtonCancel.IsEnabled = false;
+
+                NameTextBox.Text = Datalist[ListViewTreatType.SelectedIndex].Name;
+                DescriptionTextBox.Text = Datalist[ListViewTreatType.SelectedIndex].Description;
             }
         }
 
@@ -87,63 +93,65 @@ namespace WpfApplication1.CustomUI
             }
         }
 
-        private void AddButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            //throw new NotImplementedException();
-            try
-            {
-                if (this.NameTextBox.Text.Equals("") || !CheckNameIsExist(this.NameTextBox.Text))
-                {
-                    var a = new RemindMessageBox1();
-                    a.remindText.Text = (string)FindResource("Message1001"); ;
-                    a.ShowDialog();
-                    return;
-                }
-                using (var treatStatusDao = new TreatStatusDao())
-                {
-                    var treatStatus = new TreatStatus();
-                    treatStatus.Name = this.NameTextBox.Text;
-                    treatStatus.Description = this.DescriptionTextBox.Text;
-                    int lastInsertId = -1;
-                    treatStatusDao.InsertTreatStatus(treatStatus, ref lastInsertId);
-                    //UI
-                    var treatStatusData = new TreatStatusData();
-                    treatStatusData.Id = treatStatus.Id;
-                    treatStatusData.Name = treatStatus.Name;
-                    treatStatusData.Description = treatStatus.Description;
-                    Datalist.Add(treatStatusData);
-                }
-            }
-            catch (Exception ex)
-            {
-                MainWindow.Log.WriteInfoConsole("In CInfectType.xaml.cs:AddButton_OnClick exception messsage: " + ex.Message);
-            }
-        }
+        //private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    //throw new NotImplementedException();
+        //    try
+        //    {
+        //        if (this.NameTextBox.Text.Equals("") || !CheckNameIsExist(this.NameTextBox.Text))
+        //        {
+        //            var a = new RemindMessageBox1();
+        //            a.remindText.Text = (string)FindResource("Message1001"); ;
+        //            a.ShowDialog();
+        //            return;
+        //        }
+        //        using (var treatStatusDao = new TreatStatusDao())
+        //        {
+        //            var treatStatus = new TreatStatus();
+        //            treatStatus.Name = this.NameTextBox.Text;
+        //            treatStatus.Activated = (bool) this.CheckBox1.IsChecked;
+        //            treatStatus.Description = this.DescriptionTextBox.Text;
+        //            int lastInsertId = -1;
+        //            treatStatusDao.InsertTreatStatus(treatStatus, ref lastInsertId);
+        //            //UI
+        //            var treatStatusData = new TreatStatusData();
+        //            treatStatusData.Id = treatStatus.Id;
+        //            treatStatusData.Name = treatStatus.Name;
+        //            treatStatusData.Activated = treatStatus.Activated;
+        //            treatStatusData.Description = treatStatus.Description;
+        //            Datalist.Add(treatStatusData);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MainWindow.Log.WriteInfoConsole("In CInfectType.xaml.cs:AddButton_OnClick exception messsage: " + ex.Message);
+        //    }
+        //}
 
-        private void UpdateButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (ListView1.SelectedIndex == -1) return;
+        //private void UpdateButton_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    if (ListViewTreatType.SelectedIndex == -1) return;
 
-            if (this.NameTextBox.Text.Equals(""))
-            {
-                var a = new RemindMessageBox1();
-                a.remindText.Text = (string)FindResource("Message1001"); ;
-                a.ShowDialog();
-                return;
-            }
-            //throw new NotImplementedException();
-            using (var treatStatusDao = new TreatStatusDao())
-            {
-                var condition = new Dictionary<string, object>();
-                condition["ID"] = Datalist[ListView1.SelectedIndex].Id;
+        //    if (this.NameTextBox.Text.Equals(""))
+        //    {
+        //        var a = new RemindMessageBox1();
+        //        a.remindText.Text = (string)FindResource("Message1001"); ;
+        //        a.ShowDialog();
+        //        return;
+        //    }
+        //    //throw new NotImplementedException();
+        //    using (var treatStatusDao = new TreatStatusDao())
+        //    {
+        //        var condition = new Dictionary<string, object>();
+        //        condition["ID"] = Datalist[ListViewTreatType.SelectedIndex].Id;
 
-                var fileds = new Dictionary<string, object>();
-                fileds["NAME"] = NameTextBox.Text;
-                fileds["DESCRIPTION"] = DescriptionTextBox.Text;
-                treatStatusDao.UpdateTreatStatus(fileds, condition);
-                RefreshData();
-            }
-        }
+        //        var fileds = new Dictionary<string, object>();
+        //        fileds["NAME"] = NameTextBox.Text;
+        //        fileds["DESCRIPTION"] = DescriptionTextBox.Text;
+        //        treatStatusDao.UpdateTreatStatus(fileds, condition);
+        //        RefreshData();
+        //    }
+        //}
 
         private void RefreshData()
         {
@@ -160,6 +168,7 @@ namespace WpfApplication1.CustomUI
                         var treatStatusData = new TreatStatusData();
                         treatStatusData.Id = pa.Id;
                         treatStatusData.Name = pa.Name;
+                        treatStatusData.Activated = pa.Activated;
                         treatStatusData.Description = pa.Description;
                         Datalist.Add(treatStatusData);
                     }
@@ -171,20 +180,73 @@ namespace WpfApplication1.CustomUI
             }
         }
 
-        private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+
+        private void ButtonApply_OnClick(object sender, RoutedEventArgs e)
         {
-            if (ListView1.SelectedIndex == -1) return;
+
+            if (ListViewTreatType.SelectedIndex == -1) return;
+
+            //if (this.NameTextBox.Text.Equals(""))
+            //{
+            //    var a = new RemindMessageBox1();
+            //    a.remindText.Text = (string)FindResource("Message1001"); ;
+            //    a.ShowDialog();
+            //    return;
+            //}
             //throw new NotImplementedException();
             using (var treatStatusDao = new TreatStatusDao())
             {
-                treatStatusDao.DeleteTreatStatus(Datalist[ListView1.SelectedIndex].Id);
+                var condition = new Dictionary<string, object>();
+                condition["ID"] = Datalist[ListViewTreatType.SelectedIndex].Id;
+
+                var fileds = new Dictionary<string, object>();
+                fileds["NAME"] = NameTextBox.Text;
+                fileds["ACTIVATED"] = this.CheckBox1.IsChecked;
+                fileds["DESCRIPTION"] = DescriptionTextBox.Text;
+                treatStatusDao.UpdateTreatStatus(fileds, condition);
+                int temp = this.ListViewTreatType.SelectedIndex;
                 RefreshData();
+                this.ListViewTreatType.SelectedIndex = temp;
+            }
+
+
+        }
+        private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            {
+                this.ListViewTreatType.SelectedIndex = -1;
+                this.ListViewTreatType.SelectedIndex = currnetIndex;
             }
         }
+        //private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    if (ListViewTreatType.SelectedIndex == -1) return;
+        //    //throw new NotImplementedException();
+        //    using (var treatStatusDao = new TreatStatusDao())
+        //    {
+        //        treatStatusDao.DeleteTreatStatus(Datalist[ListViewTreatType.SelectedIndex].Id);
+        //        RefreshData();
+        //    }
+        //}
 
         private void CTreatStatus_OnLoaded(object sender, RoutedEventArgs e)
         {
             //throw new NotImplementedException();
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.ButtonApply.IsEnabled = true;
+            this.ButtonCancel.IsEnabled = true;
+
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            this.ButtonApply.IsEnabled = true;
+            this.ButtonCancel.IsEnabled = true;
+
         }
     }
 
@@ -193,6 +255,7 @@ namespace WpfApplication1.CustomUI
         private Int64 _id;
         private string _name;
         private string _description;
+        private bool _activated;
 
         public TreatStatusData()
         {
@@ -207,6 +270,17 @@ namespace WpfApplication1.CustomUI
                 OnPropertyChanged("Id");
             }
         }
+
+        public bool Activated
+        {
+            get { return _activated; }
+            set
+            {
+                _activated = value;
+                OnPropertyChanged("Activated");
+            }
+        }
+
 
         public string Name
         {
